5d841aa74fc04674912e970bc298d597477eece2
sultan ahmad <43011830+Sultan0ML@users.noreply.github.com>
2024-12-23 01:05:38 +0530

    Update QA.py

--- QA.py
+++ QA.py
@@ -9,7 +9,7 @@ import streamlit as st
 
 def scrap_data(url):
     """Scrape data from the given URL."""
-    loaders = AsyncChromiumLoader([url], user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
+    loaders = AsyncChromiumLoader([url])
     docs = loaders.load()
 
     bs_transformer = BeautifulSoupTransformer()
